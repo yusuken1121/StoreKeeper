7b0a956c8c45106f0d7435cb5a68ac69289c0441
yusuken1121 <yusuken1121@gmail.com>
2024-08-10 22:13:49 -0700

    ✨ Added initial Firebase settings

---
+++ backend/src/config/firebaseConfig.ts
@@ -0,0 +1,10 @@
+import * as admin from "firebase-admin";
+import serviceAccount from "../secrets/serviceAccountKey.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+
+const db = admin.firestore();
+
+export default db;
