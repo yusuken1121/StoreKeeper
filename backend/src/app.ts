9e8a372617174879f891e70fb2af5c35d36d21d5
yusuken1121 <yusuken1121@gmail.com>
2024-08-10 21:32:56 -0700

    ⚡️ Added middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 export const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.all("*", (req: Request, res: Response) => {
   res.status(404).json({ error: "Not Found Route" });
